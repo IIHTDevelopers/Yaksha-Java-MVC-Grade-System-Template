3dd57940e70a14f10e3946f19286a9001d36206d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-28 19:59:36 +0530

    Delete src directory

--- src/main/java/com/yaksha/training/grades/repository/GradeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yaksha.training.grades.repository;
-
-import com.yaksha.training.grades.entity.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Long> {
-}
